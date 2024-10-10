eb52822fc349a338e5b4e6a6d44d75e523d4a931
Carlos Daniel <carlosporrasayala@gmail.com>
2024-10-10 17:54:46 -0500

    feat | Actualizar y Eliminar Artículos

--- blog-dapper/Areas/Admin/Controllers/ArticlesController.cs
+++ blog-dapper/Areas/Admin/Controllers/ArticlesController.cs
@@ -33,7 +33,7 @@ public class ArticlesController : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Create([Bind("Title, Description, State, CategoryId, CreatedAt")]Article article)
+    public IActionResult Create([Bind("Title, Description, State, CategoryId")]Article article)
     {
         if (!ModelState.IsValid) return View(article);
         var mainPath = _hostingEnvironment.WebRootPath;
@@ -67,16 +67,38 @@ public class ArticlesController : Controller
         if (id == null) return NotFound();
         var article = _articleRepository.GetArticle(id.GetValueOrDefault());
         if (article == null) return NotFound();
-        ViewBag.Categories = _categoryRepository.GetCategories();
+        ViewBag.Categories = _categoryRepository.GetListCategories();
         return View(article);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Edit(int id, [Bind("ArticleId, Title, Content, CategoryId")] Article article)
+    public IActionResult Edit(int id, [Bind("ArticleId, Title, Description, Image, State, CategoryId")] Article article)
     {
         if (id != article.ArticleId) return NotFound();
         if (!ModelState.IsValid) return View(article);
+        
+        var mainPath = _hostingEnvironment.WebRootPath;
+        var files = HttpContext.Request.Form.Files;
+        if (files.Count > 0)
+        {
+            var fileName = Guid.NewGuid().ToString();
+            var uploads = Path.Combine(mainPath, @"images\articles");
+            var extension = Path.GetExtension(files[0].FileName);
+            if (article.Image != null)
+            {
+                var imagePath = Path.Combine(mainPath, article.Image.TrimStart('\\'));
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+
+            using var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create);
+            files[0].CopyTo(fileStreams);
+            article.Image = @"\images\articles\" + fileName + extension;
+        }
+        
         _articleRepository.UpdateArticle(article);
         return RedirectToAction(nameof(Index));
     }
